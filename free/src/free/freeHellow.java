c070b62a79998db9d467dbf9632b67dec4a63f25
User <User@DESKTOP-VM4JJJB>
2021-02-15 23:09:35 +0900

    깃허브 연습파일

---
+++ free/src/free/freeHellow.java
@@ -0,0 +1,14 @@
+package free;
+
+public class freeHellow {
+
+	public freeHellow() {
+		System.out.println("hello java");
+	}
+
+	public static void main(String[] args) {
+		new freeHellow();
+
+	}
+
+}
